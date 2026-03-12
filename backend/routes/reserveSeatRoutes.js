1482852711c4b8888e8ca2fbe35ca02185e64ed4
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2026-03-12 18:12:13 +0800

    feat: creating POST for reserving seat/s - (WIP) conflict handling. Planning to do double checking upon submit button.

--- backend/routes/reserveSeatRoutes.js
+++ backend/routes/reserveSeatRoutes.js
@@ -26,7 +26,7 @@ router.get('/api/get-seats', (req,res)=>{
 
     let takenSeats = [];
     
-    takenSeats.forEach(user =>{
+    booked.forEach(user =>{
         user.reservation.forEach(rsv => {
             if(rsv.details.room === room && rsv.details.schedule === schedule){
                 takenSeats.concat(resv.details.seats);
@@ -34,4 +34,26 @@ router.get('/api/get-seats', (req,res)=>{
         })
     })
     res.json(takenSeats);
+});
+
+
+// Reserve the seats
+// - Handle conflicts
+router.post('/api/reserve', (req,res)=>{
+    let {schedule, room, seats} = req.body;
+    reservation.dt_request = new Date();
+
+    const conflict = db.instance.collection("Users").find({
+        "reservation.details.schedule": schedule,
+        "reservation.details.room": room,
+        "reservation.details.seats": {$in: [seats]}
+    });
+
+    if(conflict){
+        return res.status(409).json({message: "Seat/s already taken by another user. Please refresh"});
+    }
+
+    let result = db.instance.collection("Users").insertOne(reservation);
+    result.save();
+    res.status(201).json({message: "Success"});
 });
