807d61e3f036a35c6d93fe7cff6fb0a99ba7c635
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2026-03-12 18:53:26 +0800

    feat: modified routing of each bldg - rooms and time are now dynamically injected into the hbs

--- backend/routes/reserveSeatRoutes.js
+++ backend/routes/reserveSeatRoutes.js
@@ -2,58 +2,102 @@ const express = require('express');
 const router = express.Router();
 const hbs = require('../hbs');
 
-module.exports = router;
 
 // Load Page
-router.get('/', (req,res) => {
+router.get('/gokongwei', (req,res) => {
     try {
-        res.render('reserve-seat', hbs.getTemplate('reserve-seat'));
+        let roomsList = ["G302", "G303", "G304", "G305", "G306"];
+        const timesList  = [
+            "0730", "0800", "0830", "0900", "0930", 
+            "1000", "1030", "1100", "1130", "1200", 
+            "1230", "1300", "1330", "1400", "1430", 
+            "1500", "1530", "1600", "1630", "1700"
+        ];
+        res.render('reserve-seat', {
+            rooms: roomsList,
+            time: timesList
+        });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+router.get('/henrysy', (req,res) => {
+    try {
+        let roomsList = ["H802", "H803", "H901", "H1001", "H1002"];
+        const timesList  = [
+            "0730", "0800", "0830", "0900", "0930", 
+            "1000", "1030", "1100", "1130", "1200", 
+            "1230", "1300", "1330", "1400", "1430", 
+            "1500", "1530", "1600", "1630", "1700"
+        ];
+        res.render('reserve-seat', {
+            rooms: roomsList,
+            time: timesList
+        });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+router.get('/stlasalle', (req,res) => {
+    try {
+        let roomsList = ["L220", "L229", "L230", "L319", "L320"];
+        const timesList  = [
+            "0730", "0800", "0830", "0900", "0930", 
+            "1000", "1030", "1100", "1130", "1200", 
+            "1230", "1300", "1330", "1400", "1430", 
+            "1500", "1530", "1600", "1630", "1700"
+        ];
+        res.render('reserve-seat', {
+            rooms: roomsList,
+            time: timesList
+        });
     } catch (error) {
         res.status(500).send(error.message);
     }
 });
 
+module.exports = router;
 
-// Get Available Seats
-router.get('/api/get-seats', (req,res)=>{
-    // query in reserve-seat.js with "/api/get-seats"
-    const {schedule, room} = req.query;
+// // Get Available Seats
+// router.get('/api/get-seats', (req,res)=>{
+//     // query in reserve-seat.js with "/api/get-seats"
+//     const {schedule, room} = req.query;
 
-    const booked = db.instance.collection("Users").find({
-        "reservation.details.schedule": schedule, 
-        "reservation.details.room": room})
-        .toArray();
+//     const booked = db.instance.collection("Users").find({
+//         "reservation.details.schedule": schedule, 
+//         "reservation.details.room": room})
+//         .toArray();
 
-    let takenSeats = [];
+//     let takenSeats = [];
     
-    booked.forEach(user =>{
-        user.reservation.forEach(rsv => {
-            if(rsv.details.room === room && rsv.details.schedule === schedule){
-                takenSeats.concat(resv.details.seats);
-            }
-        })
-    })
-    res.json(takenSeats);
-});
+//     booked.forEach(user =>{
+//         user.reservation.forEach(rsv => {
+//             if(rsv.details.room === room && rsv.details.schedule === schedule){
+//                 takenSeats.concat(resv.details.seats);
+//             }
+//         })
+//     })
+//     res.json(takenSeats);
+// });
 
 
-// Reserve the seats
-// - Handle conflicts
-router.post('/api/reserve', (req,res)=>{
-    let {schedule, room, seats} = req.body;
-    reservation.dt_request = new Date();
+// // Reserve the seats
+// // - Handle conflicts
+// router.post('/api/reserve', (req,res)=>{
+//     let {schedule, room, seats} = req.body;
+//     reservation.dt_request = new Date();
 
-    const conflict = db.instance.collection("Users").find({
-        "reservation.details.schedule": schedule,
-        "reservation.details.room": room,
-        "reservation.details.seats": {$in: [seats]}
-    });
+//     const conflict = db.instance.collection("Users").find({
+//         "reservation.details.schedule": schedule,
+//         "reservation.details.room": room,
+//         "reservation.details.seats": {$in: [seats]}
+//     });
 
-    if(conflict){
-        return res.status(409).json({message: "Seat/s already taken by another user. Please refresh"});
-    }
+//     if(conflict){
+//         return res.status(409).json({message: "Seat/s already taken by another user. Please refresh"});
+//     }
 
-    let result = db.instance.collection("Users").insertOne(reservation);
-    result.save();
-    res.status(201).json({message: "Success"});
-});
+//     let result = db.instance.collection("Users").insertOne(reservation);
+//     result.save();
+//     res.status(201).json({message: "Success"});
+// });
