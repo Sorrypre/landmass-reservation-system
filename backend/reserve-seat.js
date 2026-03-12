a19c17b05a18ae11409b7ff28965fcd8373684ce
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2026-03-12 18:51:55 +0800

    fix: added bldgname getter based on previous page

--- backend/reserve-seat.js
+++ backend/reserve-seat.js
@@ -1,18 +1,24 @@
+let bldgname = localStorage.getItem("bldg-name");
+let bldg_text = document.querySelector(".reserve-bldg-text");
+if (bldgname && bldg_text) {
+	bldg_text.innerHTML = bldgname;
+}
+
 let roomname = localStorage.getItem("room-name");
 let room_text = document.querySelector(".reserve-room-text");
-if(roomname && room_text){
+if (roomname && room_text) {
 	room_text.innerHTML = roomname;
 }
 
 let go_back = document.querySelector("#go-back-container .go-back-text");
-go_back.onclick = function (){
+go_back.onclick = function () {
 	window.location.href = "/rr";
 };
 
 let container = document.getElementById("slots-container");
 
 container.addEventListener("click", (e) => {
-	if(e.target.classList.contains('seat')){
+	if (e.target.classList.contains('seat')) {
 		console.log("Seat clicked:", e.target.dataset.pc);
 		e.target.classList.toggle("selected");
 	}
@@ -32,6 +38,10 @@ function closeModal() {
 	modal.classList.add('hidden');
 }
 
-function returnDashboard(){
+function returnDashboard() {
 	window.location.href = '/dashboard';
 }
+
+let date = document.$("#reserved-date");
+let time = document.$("#reserved-time");
+let room = document.$("#reserved-room")
